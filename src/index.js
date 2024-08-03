d89077a5cdf9c643b92534af7b60aab045940c32
Vlad Bezsonov <vd115313@gmail.com>
2024-08-03 18:05:33 +0200

    feat: resolved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+ module.exports = function reverse (n) {
+    return Number(n.toString().split('').filter((e) => e !== '-').reverse().join(''))
 }
+
+
+// reverse(123)// 321
+// reverse(233); // 332
+// reverse(535); // 535
+// reverse(95034); // 43059
+
+
+// console.log(reverse(-5243));
